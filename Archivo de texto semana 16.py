f093b8e06fcb71bf08b4df53041bb3cc67c76e76
Tania Deber Ej <tg.guamang@uea.edu.ec>
2024-10-05 22:57:46 -0500

    Desarrollo de archivo de texto

---
+++ Archivo de texto semana 16.py
@@ -0,0 +1,23 @@
+# Escritura de Archivo de Texto
+archivo = open("my_notes.txt", "w")  # Abrir archivo en modo escritura
+
+# Escribe al menos tres líneas de notas personales
+archivo.write("Nota 1: Recuerda tomar tus pastillas hoy.\n")
+archivo.write("Nota 2: Las clases empiezan a las 7 am.\n")
+archivo.write("Nota 3: No olvides hacer tus tareas.\n")
+archivo.write("Nota 4: Recuerda lavar tu uniforme.\n")
+archivo.write("Nota 5: No olvides dormir temprano.\n")
+
+archivo.close()  # Cerrar archivo
+
+
+# Lectura de Archivo de Texto
+archivo = open("my_notes.txt", "r")  # Abrir archivo en modo lectura
+
+# Lee el contenido del archivo línea por línea
+linea = archivo.readline()
+while linea:
+    print(linea.strip())  # Muestra en la consola cada línea leída
+    linea = archivo.readline()
+
+archivo.close()  # Cerrar archivo
